5a191cfd9fe584193f80f6569a94fe08f24e86b0
Ivan <grubi.ivan@gmail.com>
2018-12-20 13:37:59 +0100

    Bug and UI fixes

---
+++ app/src/main/java/app/tennispartner/tennispartner/ShareActivity.java
@@ -0,0 +1,13 @@
+package app.tennispartner.tennispartner;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+    }
+}
